df2c0e4e2a31dabf81b1212bbf2dd056914f5b2c
Ambition Kyndiah <53797801+ambiknds@users.noreply.github.com>
2024-08-30 00:24:48 +0000

    update of file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-angular-project';
+  productName = new FormControl('', [Validators.required, Validators.minLength(5)])
+  productPrice = new FormControl('', [Validators.required, Validators.min(10)])
+  productCategory = new FormControl('', [Validators.required])
+
+  productForm = new FormGroup({
+    productName: this.productName,
+    productPrice: this.productPrice,
+    productCategory: this.productCategory
+  })
+
+  submitForm(){
+    event?.preventDefault();
+    if(this.productForm.valid == true){
+      // console.log(this.productName, this.productPrice, this.productCategory)
+      console.log(this.productForm.value)
+    }    
+  }
 }
